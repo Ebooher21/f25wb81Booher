bcc22cc66426b3ec79e6cb79590dbec85811d8e1
Elijah Booher <99350249+Ebooher21@users.noreply.github.com>
2025-11-02 17:13:13 -0600

    Part 9: added randomitem.pug and pick.js, opening properly

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kangarooRouter = require('./routes/kangaroo');
+var gridRouter = require('./routes/grid');
+var itemRouter = require('./routes/pick');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kangaroo', kangarooRouter);
+app.use('/grid', gridRouter);
+app.use('/pick', itemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
